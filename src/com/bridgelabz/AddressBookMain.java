b937ca358fded6432b75235b729870e5ddead8b8
S MD ASJAD AZMI <asjadazmi0786@gmail.com>
2022-06-01 17:50:23 +0530

    Added:UC2_User_Input

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -1,10 +1,19 @@
 package com.bridgelabz;
 
-public class AddressBookMain{
-	public static void main(String[] args) {
-		System.out.println("welcome to the Adress book Program");
-		Contact c1=new Contact(first_name,last_name,Address,city,state,phone_number,email); 
-		System.out.println("my name is :"+c1.first_name+" "+c1.last_name+"\nAddress: "+c1.Address+".\nmy city :"+c1.city+"\nmy state"+c1.state+"\nphone number:"+c1.phone_number+"\nemail"+c1.email);
-		
-	}
+public class AddressBookMain {
+	public static String first_name;
+	public static String last_name;
+	public static String Address;
+	public static String city;
+	public static String state;
+	public static int zip;
+	public static long phone_number;
+	public static String email;
+
+}
+	
+	
+	
+	
+
 }
